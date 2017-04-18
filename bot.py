6b10ecb34f956f79eba50ecb2b0c00c618da0be7
Cberdaguer <charles.berdaguer@imerir.com>
2017-04-18 13:53:14 +0200

    Ajout de la commande pascalmoule

--- bot.py
+++ bot.py
@@ -75,6 +75,10 @@ async def on_message(message):
         await client.send_message(message.channel, 'Vous êtes un Cassos ? Vous avez abandonnez votre gamin dans un foyer car vous étiez trop bourré pour vous en occuper ? Il vous manque la moitié des dents et vous portez des chemises dragons ? N\'attendez plus et appelez nous. Nous sommes la pour vous ridiculisez devant la France entière et ce moquer de votre consanguinité élévé ! Contactez nous à : niquetamerelecassosetfaisnousdelaudience@nrj12.fdp.com')
 
     elif message.content.startswith('!pascalMoule'):
-        await client.send_message(message.channel, '!randmoule')
+        i = 0
+        while i < 5:
+            await client.send_message(message.channel, '!randmoule')
+            await asyncio.sleep(2)
+            i += 1
 
 client.run(token)
